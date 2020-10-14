a49e764bec351275284a54cb05f348640da9403e
EliotHerbst <39841180+EliotHerbst@users.noreply.github.com>
2020-10-13 23:01:43 -0500

    Updates

---
+++ precache-manifest.336de7f6465dba7622d6058e108852c3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091c94dd6fca5c58f86c83e75f1000dc",
+    "url": "/KalaLoadingPage/index.html"
+  },
+  {
+    "revision": "e8b2f17da1032a068d62",
+    "url": "/KalaLoadingPage/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d3b3f5d6a0739fcbac8c",
+    "url": "/KalaLoadingPage/static/css/main.ceab1082.chunk.css"
+  },
+  {
+    "revision": "e8b2f17da1032a068d62",
+    "url": "/KalaLoadingPage/static/js/2.e11ba8b6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/KalaLoadingPage/static/js/2.e11ba8b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b3f5d6a0739fcbac8c",
+    "url": "/KalaLoadingPage/static/js/main.0f361482.chunk.js"
+  },
+  {
+    "revision": "f1d0f6c723f4af06126b",
+    "url": "/KalaLoadingPage/static/js/runtime-main.d18f0d0d.js"
+  },
+  {
+    "revision": "5d6ea38a769498dfc19fe6389d14db39",
+    "url": "/KalaLoadingPage/static/media/FacebookLogo.5d6ea38a.png"
+  },
+  {
+    "revision": "a0ad143e4bb4db3a78d3a2b99b5e5d6e",
+    "url": "/KalaLoadingPage/static/media/Kala.a0ad143e.svg"
+  },
+  {
+    "revision": "5909b1dff0733e043d3c8f4d914c0102",
+    "url": "/KalaLoadingPage/static/media/background.5909b1df.jpg"
+  },
+  {
+    "revision": "c15b915284e0eb6ace7fd850ec809fe2",
+    "url": "/KalaLoadingPage/static/media/instagram.c15b9152.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8a0fdd0dc102ad5327bfd8a64e6f5db.js"
+  "/KalaLoadingPage/precache-manifest.336de7f6465dba7622d6058e108852c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KalaLoadingPage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
